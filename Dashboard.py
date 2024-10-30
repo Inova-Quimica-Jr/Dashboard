8436fe89a058cafa736ad35f33f5c66c96b1005e
Inova-Quimica-Jr <iqjr@iq.usp.br>
2024-10-29 21:02:10 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -23,16 +23,15 @@ col1, col2, col3 = st.columns([1, 1, 1])
 url = "https://api.pipefy.com/graphql"
 
 # URL base do Excel da Gameficação
-url_game = 'https://drive.google.com/uc?id=1szuXLsXhHZPELuTAo5ClL8Ez2fOn422R'
-dados_game = pd.read_excel(url_game, header=4)
-dados_game.rename(columns={'Total': 'Pontuação Departamentos', 'Total.1': 'Pontuação Individual'}, inplace=True)
-dados_game.drop(['Unnamed: 0', 'Unnamed: 3','Unnamed: 4' ], axis=1, inplace=True)
+url_game = 'https://drive.google.com/uc?id=1szuXLsXhHZPELuTAo5ClL8Ez2fOn422R' #Caso precise atualizar este link tomar cuidado para não colocar o link do documento na forma editavel
+dados_game = pd.read_excel(url_game, header=4) # Essas configurações irão depender da formatação feina no excel
+dados_game.rename(columns={'Total': 'Pontuação Departamentos', 'Total.1': 'Pontuação Individual'}, inplace=True) # Isso renomeia as colunas para melhor compreensão para codar
+dados_game.drop(['Unnamed: 0', 'Unnamed: 3','Unnamed: 4' ], axis=1, inplace=True) # Exclui colunas desnecessarias
 
 # Cabeçalhos com a chave de autenticação
 headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3MjUyOTIyNDgsImp0aSI6ImM0OTNlYjllLTc4ZTUtNGYxYy05MGQ0LTI3NDNkNGNhYzRkZiIsInN1YiI6NzYyNDU1LCJ1c2VyIjp7ImlkIjo3NjI0NTUsImVtYWlsIjoiaXFqckBpcS51c3AuYnIifX0.Kj8wdVuN7nlyQgaEuMXGnep8hkDovLlsrf8-AsD_c6pzZoeK2vUvtkn0Z1uk6ptsPOf6ZUszTdsswlIMkmLOgg",
-    "Content-Type": "application/json"
-}
+    "Content-Type": "application/json"}
 
 # Função para obter os dados do pipefy da equipe de projetos
 def obter_cards_do_pipe_do_projetos(url, headers):
@@ -237,14 +236,14 @@ def gerar_grafico_distribuicao(df, coluna, titulo=''):
     # Exibir o gráfico
     st.pyplot(fig)
 
-def gerar_grafico_barras(data, x_col, y_col, title='', x_label='', y_label='Pontos', color='skyblue',title_fontsize=16, label_fontsize=14, rotation=0):
+#Esta função gera um grafico de barras, As informações que constam entre paraenteses devem ser dados ao chamar a função, caso contrario utilizara o padrão que esta setado( isso não se aplica aos tres primeiros parametros, estes são mandatorios).
+def gerar_grafico_barras(data, x_col, y_col, title='', x_label='', y_label='Pontos', color='skyblue',title_fontsize=20, label_fontsize=14, rotation=0):
 
-    fig, ax = plt.subplots(figsize=(10, 6))  # Definir o tamanho da figura
+    fig, ax = plt.subplots(figsize=(10, 8))  # Definir o tamanho da figura
 
     # Criar o gráfico de barras
     ax.bar(data[x_col].dropna().astype(str), data[y_col].dropna(), color=color)
 
-    # Definir fundo transparente
     # Definir fundo transparente
     fig.patch.set_alpha(0)    # Fundo da figura transparente
     ax.set_facecolor('none')  # Fundo do gráfico transparente
@@ -260,6 +259,56 @@ def gerar_grafico_barras(data, x_col, y_col, title='', x_label='', y_label='Pont
     # Exibir o gráfico
     st.pyplot(fig)
 
+def gerar_ranking(data,x_col,y_col, title='Pódio', x_label='', y_label='Pontuação', title_fontsize=20, label_fontsize=18,):
+    
+    data = data.nlargest(3,data[y_col].name)
+
+    # Reindexar para centralizar o maior
+    data = data.reset_index(drop=True)
+    data = data.reindex([1, 0, 2])
+    
+    # Cores para as barras
+    colors = ['silver', 'gold', 'saddlebrown']
+    
+    # Criar o gráfico
+    fig, ax = plt.subplots(figsize=(10, 8))
+    
+    fig.patch.set_alpha(0)    # Fundo da figura transparente
+    ax.set_facecolor('none')  # Fundo do gráfico transparente
+    ax.grid(False)            # Fundo sem grades
+
+    # Adicionar título e rótulos com texto branco e tamanhos de fonte ajustáveis
+    ax.set_title(title, color='white', fontsize=title_fontsize)
+    ax.set_xlabel(x_label, color='white', fontsize=label_fontsize)
+    ax.set_ylabel(y_label, color='white', fontsize=label_fontsize)
+    ax.tick_params(axis='x', colors='white', labelsize=label_fontsize)  # Cor e tamanho dos rótulos do eixo X
+    ax.tick_params(axis='y', colors='white', labelsize=label_fontsize)  # Cor e tamanho dos rótulos do eixo Y
+    plt.xticks(color='white', fontsize=label_fontsize)
+    plt.xticks(color='white')
+
+    # Criar o gráfico de barras
+    bars = plt.bar(data[x_col], data[y_col], 
+                   color=colors, width=0.6)
+
+    # Adicionar os valores no topo das barras
+    for bar in bars:
+        plt.text(bar.get_x() + bar.get_width() / 2, 
+                 bar.get_height() + 2,  # Posição ligeiramente acima da barra
+                 str(int(bar.get_height())), 
+                 ha='center', va='bottom', fontsize=15, fontweight='bold', color='white')
+    
+    # Mostrar o gráfico
+    st.pyplot(fig)
+
+def Faz_tabela_de_Posição(data,x_col,y_col):
+    data = data.sort_values(by= data[y_col].name,ascending=False).reset_index(drop=True)
+    data[y_col] = data[y_col].round(2)
+    data['Posição'] = data.index + 1
+    data['Posição'] = data['Posição'].astype(str) + '° lugar'
+    Tabela_Posição = data[[x_col,y_col,'Posição']].dropna()
+    st.dataframe(Tabela_Posição.reset_index(drop=True), use_container_width=True, hide_index=True)
+    
+
 with col1:
     Opções = ['Vendas', 'Gameficação', 'Projetos em Andamento']
     Visualização = st.selectbox('Escolha sobre o que deseja ver', Opções
@@ -274,7 +323,9 @@ with col1:
                         .index
                         .tolist())
         df_filtrado = df_vendas[df_vendas['Tipo de Serviço'].str.contains(Projetos, na=False, regex=False)]
-        st.table(df_filtrado[['Title', 'Valor fechado']])
+        st.dataframe(df_filtrado[['Title', 'Valor fechado']], hide_index=True)
+
+
 
 with col2: 
     if Visualização == 'Vendas':
@@ -282,14 +333,15 @@ with col2:
 
 
     elif Visualização == 'Gameficação':
-       gerar_grafico_barras(dados_game,'Departamentos', 'Pontuação Departamentos', 'Pontuação Departamentos' )
         
+        gerar_grafico_barras(dados_game,'Nomes', 'Pontuação Individual', 'Pontuação Individual', rotation=90 )
+        
+        gerar_grafico_barras(dados_game,'Departamentos', 'Pontuação Departamentos', 'Pontuação Departamentos')
 
 
     elif Visualização == 'Projetos em Andamento':
         df_filtrado = df_projetos[df_projetos['Phase'].str.contains('Execução', na=False, regex=False)]
-        st.table(df_filtrado[['Title', 'Descrição']])
-        
+        st.table(df_filtrado[['Title', 'Descrição']], hide_index=True)
 
 
 with col3:
@@ -297,7 +349,8 @@ with col3:
         st.write('Em construção')
 
     elif Visualização == 'Gameficação':
-        gerar_grafico_barras(dados_game,'Nomes', 'Pontuação Individual', 'Pontuação Individual', rotation=90 )
+        Faz_tabela_de_Posição(dados_game,'Nomes', 'Pontuação Individual')
+        Faz_tabela_de_Posição(dados_game,'Departamentos', 'Pontuação Departamentos')
 
     elif Visualização == 'Projetos em Andamento':
         st.write('Em construção')
